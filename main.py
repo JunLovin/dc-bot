5e9f7b6722ac5d4e11840f8e7e7a3d1e233b7923
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-27 13:44:20 -0500

    refactor: clean up main.py by removing unused imports and organizing code structure

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-# Importo discord
 import discord
-# Importo commands de discord
 from discord.ext import commands
-# Importo os junto con dotenv para poder usar las variables de entorno
 import os
 from dotenv import load_dotenv
-# Importo genai
-from google import genai
-# Importo mi función getResponse para separar el código
-from utils import getResponse
+import asyncio
 
-# . Cargo las variables de entorno
 load_dotenv()
 
-# . Creo un objeto intents y le digo que quiero usar todos los intents
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
 intents = discord.Intents.all()
-# . Le digo que quiero usar el intent de message content
 intents.message_content = True
 
-# . Creo un objeto bot y le digo que el prefijo es $ y que quiero usar los intents
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-# - Creo un comando que se ejecuta cuando se escribe $gemini
-@bot.command(name="gemini")
-async def gemini(ctx, *args):
-    response = getResponse(args[0])
-    await ctx.send(response)
+initial_extensions = [
+    'cogs.gemini_cog',
+    'cogs.moderator_cog',
+    'cogs.basic_cog'
+]
 
-# - Creo un evento que se ejecuta cuando el bot esta listo
 @bot.event
-async def on_ready():
+async def on_ready() -> None:
     print(f"Bot logueado como: {bot.user}")
 
-# . Ejecuto el bot con el token
-bot.run(os.getenv('BOT_TOKEN'))
+    for extension in initial_extensions:
+        try:
+            await bot.load_extension(extension)
+            print(f"Cog {extension} cargado correctamente")
+        except Exception as e:
+            print(f"Error al cargar el cog {extension}: {e}")
+
+
+
+bot.run(BOT_TOKEN)
