9f98af90c14688894e20ee68f79b27ff91c820c3
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-27 13:44:34 -0500

    fix: update Gemini model version and correct content formatting in getResponse function

--- utils.py
+++ utils.py
@@ -2,20 +2,18 @@ from google import genai
 import os
 from dotenv import load_dotenv
 
-# . Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
-# . Guardo la API key de Gemini
 geminiKey = os.getenv('GEMINI_TOKEN')
-#. Creo un objeto cliente de genai y le paso la API key
+#. Creo un cliente
 client = genai.Client(api_key=f'{geminiKey}')
 
 # - Función que devuelve la respuesta de Gemini y acepta un prompt como parámetro
 def getResponse(prompt: str) -> str:
     try: 
         response = client.models.generate_content(
-        model = "gemini-2.0-flash",
-        contents = prompt + "Dilo en máximo 2000 carácteres"
+        model = "gemini-1.5-flash",
+        contents = prompt + " Dilo en máximo 2000 caracteres"
     )
     except Exception as e:
         return "Error: " + str(e)
